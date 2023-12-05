2641aa63ff7e50dbc4048aacc2600a8b6f9b6516
dracarys18 <karthikey.hegde@juspay.in>
2023-12-05 15:26:24 +0530

    feat: day4 part2

--- day4/day4.go
+++ day4/day4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang-collections/collections/stack"
 	"math"
 	"slices"
 	"strconv"
@@ -10,9 +11,10 @@ import (
 )
 
 type Solve interface {
-	solve([]string) int
+	solve([]Cards) int
 }
 type Cards struct {
+	no     int
 	array1 []int
 	array2 []int
 }
@@ -47,14 +49,22 @@ func extractInt(numbers []string) ([]int, []int) {
 	arr2 := MaptoInt(numbers[1])
 	return arr1, arr2
 }
+
+func extractCardNo(s string) int {
+	card := strings.Split(s, "Card")
+	number, _ := strconv.Atoi(strings.TrimSpace(card[1]))
+	return number
+}
+
 func parse(lines []string) []Cards {
 	var card []Cards
 	for _, line := range lines {
 		game := strings.Split(line, ": ")
 		if len(game) != 1 {
+			id := extractCardNo(game[0])
 			numbers := strings.Split(game[1], "|")
 			arr1, arr2 := extractInt(numbers)
-			card = append(card, Cards{array1: arr1, array2: arr2})
+			card = append(card, Cards{id, arr1, arr2})
 		}
 	}
 	return card
@@ -68,8 +78,7 @@ func double(size int) int {
 	return result
 }
 
-func (_ Solution1) solve(lines []string) int {
-	cards := parse(lines)
+func (_ Solution1) solve(cards []Cards) int {
 	total := 0
 	for _, card := range cards {
 		intersection := card.Intersection()
@@ -78,10 +87,40 @@ func (_ Solution1) solve(lines []string) int {
 	return total
 }
 
+func (_ Solution2) solve(cards []Cards) int {
+	result := 0
+	gameStack := stack.New()
+	points := map[int]int{}
+	commons := map[int]int{}
+
+	for _, card := range cards {
+		common := card.Intersection()
+		points[card.no] = 0
+		commons[card.no] = len(common)
+		gameStack.Push(card.no)
+	}
+
+	for gameStack.Len() > 0 {
+		card := gameStack.Pop().(int)
+		points[card] = points[card] + 1
+		common := commons[card]
+		for i := 1; i <= common; i++ {
+			gameStack.Push(card + i)
+		}
+	}
+
+	for _, point := range points {
+		result = point + result
+	}
+	return result
+}
+
 func parseAndSolve[T Solve](problem T, filename string) int {
 	lines := utils.ReadLine(filename)
-	return problem.solve(lines)
+	cards := parse(lines)
+
+	return problem.solve(cards)
 }
 func main() {
-	fmt.Printf("Solution1: %d\n", parseAndSolve(Solution1{}, "./day4/input.txt"))
+	fmt.Printf("Solution1: %d\nSolution2: %d", parseAndSolve(Solution1{}, "./day4/input.txt"), parseAndSolve(Solution2{}, "./day4/input.txt"))
 }
